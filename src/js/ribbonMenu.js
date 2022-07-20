b4fb3b02d93239dae25f6391d21b8c2c5dfcfd0d
Aleksandr <104426761+nemirovka28@users.noreply.github.com>
2022-07-20 11:04:24 +0300

    fixed ribbonMenu

--- src/js/ribbonMenu.js
+++ src/js/ribbonMenu.js
@@ -13,14 +13,14 @@ export default class RibbonMenu {
 		this.elem = createElement(`
 		<div class="ribbon">
 			<button class="ribbon__arrow ribbon__arrow_left ribbon__arrow_visible ">
-				<img src="../../assets/images/icons/angle-icon.svg" alt="icon">
+				<img src="./assets/images/icons/angle-icon.svg" alt="icon">
 			</button>
 
 			<nav class="ribbon__inner">
 			</nav>
 
 			<button class="ribbon__arrow ribbon__arrow_right ribbon__arrow_visible">
-				<img src="../../assets/images/icons/angle-icon.svg" alt="icon">
+				<img src="./assets/images/icons/angle-icon.svg" alt="icon">
 			</button>
 		</div>
 
@@ -28,8 +28,6 @@ export default class RibbonMenu {
 
 		let categories = this.categories.map(item => createElement(`
 			<a href="#" class="ribbon__item" data-id="${item.id}">${item.name}</a>`))
-
-		// console.log(...categories)
 		this.sub('inner').append(...categories);
 
 	}
